1b40377cef64ac65b75ed8ddaa1116e7185507b1
Luiz Zheng <zl910627@yahoo.com>
2023-09-11 15:57:13 +0700

    - v2 api implemented - enhanced code structure

--- index.js
+++ index.js
@@ -1,125 +1,170 @@
-const axios = require('axios').default;
-const { Method } = require('axios');
 const crypto = require('crypto');
+const _ = require("lodash");
+const axios = require('axios');
 
 const STATES = [
-    'Transaction expected',
-    'The transaction is waiting for the required number of confirmations',
-    'Currency exchange',
-    'Sending funds',
-    'Completed',
-    'Expired',
-    'Not currently in use',
-    'A decision must be made to proceed with the order'
+  'Transaction expected',
+  'The transaction is waiting for the required number of confirmations',
+  'Currency exchange',
+  'Sending funds',
+  'Completed',
+  'Expired',
+  'Not currently in use',
+  'A decision must be made to proceed with the order'
 ];
 
-const resolveState = (data) => {
-    if (data.status) data.statusText = STATES[data.status];
-    return data;
-}
-
-const currencyAppend = (name, body, data) => {
-    if (data.indexOf(' ') !== -1) {
-        const arr = data.split(' ');
-        body.append(`${name}Qty`, arr[0]);
-        body.append(`${name}Currency`, arr[1]);
-    } else body.append(`${name}Currency`, data);
-}
-
 class FixedFloat {
-    /**
-     * Main API class
-     * @param {String} apiKey API key
-     * @param {String} secretKey Secret key
-     * @description Get your pair of keys from https://fixedfloat.com/apikey
-     */
-    constructor(apiKey, secretKey) {
-        if (!apiKey || !secretKey) throw new Error('Please provide an API and secret keys');
-        this.mainURL = 'https://fixedfloat.com/api/v1/';
-        this.apiKey = apiKey;
-        this.secretKey = secretKey;
+  /**
+   * Main API class
+   * @param {String} apiKey API key
+   * @param {String} secretKey Secret key
+   * @param {{refcode, afftax}} affiliate reference code.
+   * @description Get your pair of keys from https://fixedfloat.com/apikey
+   */
+  constructor(apiKey, secretKey, affiliate = undefined) {
+    if (!apiKey || !secretKey) throw new Error('Please provide an API and secret keys');
+    this._client = axios.create({
+      baseURL: 'https://fixedfloat.com/api/v2/',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'X-API-KEY': apiKey
+      }
+    })
+    this._client.interceptors.request.use((config) => {
+      const bodyStr = _.isEmpty(config.data) ? '' : JSON.stringify(config.data);
+      _.set(
+        config.headers,
+        'X-API-SIGN',
+        crypto.createHmac('sha256', Buffer.from(secretKey))
+          .update(bodyStr)
+          .digest('hex')
+      );
+      return config
+    });
+    this._client.interceptors.response.use((resp) => {
+      const data = _.get(resp, 'data.data');
+      const {status, msg, code} = data;
+      if (code !== 0 || msg !== 'OK') {
+        throw new Error(`Error ${code}: ${msg}`);
+      }
+      if (!_.isEmpty(status)) {
+        _.set(resp, 'data.data.statusText', STATES[status]);
+      }
+      return resp;
+    });
+
+    if (affiliate !== undefined) {
+      this.refcode = affiliate.refcode;
+      this.afftax = affiliate.afftax;
+    }
+  }
+
+  _splitCcy(ccyData) {
+    if (!_.includes(ccyData, ' ')) {
+      return {ccy: ccyData};
+    }
+    const [amount, ccy] = _.split(ccyData, ' ', 2);
+    return {amount, ccy};
+  }
+
+  _calcAmtAndDir(fromAmt, toAmt) {
+    if (!_.isEmpty(fromAmt)) {
+      return {amount: fromAmt, direction: 'from'};
+    }
+    if (!_.isEmpty(toAmt)) {
+      return {amount: toAmt, direction: 'to'};
+    }
+    return {};
+  }
+
+  /**
+   * Getting a list of all currencies that are available on FixedFloat.com.
+   */
+  async getCurrencies() {
+    const {data} = await this._client.post('/ccies');
+    return data;
+  }
+
+  /**
+   * Information about a currency pair with a set amount of funds.
+   * @param {String} from From currency (ex. 0.1 ETH)
+   * @param {String} to To currency (ex. BTC)
+   * @param {'fixed'|'float'} type Order type: fixed or float (def. float)
+   */
+  async getPrice(from, to, type = 'float') {
+    if (!from || !to || from.indexOf(' ') + to.indexOf(' ') === -2) {
+      throw new Error(`No required params. Example: {from: '0.1 ETH', to: 'BTC'}`);
     }
 
-    /**
-     * Made request
-     * @param {Method} req_method Request method
-     * @param {String} api_method API method
-     * @param {String} body Body or query
-     */
-    async _request(req_method, api_method, body = '') {
-        if (!req_method || !api_method) throw new Error(`Required params: req_method & api_method`)
-        const { data } = await axios({
-            method: req_method,
-            url: this.mainURL + api_method + (req_method === 'GET' ? `?${body}` : ''),
-            headers: {
-                'X-API-KEY': this.apiKey,
-                'X-API-SIGN': crypto.createHmac('sha256', Buffer.from(this.secretKey)).update(body).digest('hex')
-            },
-            data: req_method === 'GET' ? '' : body
-        });
-        if (data.code !== 0 || data.msg !== 'OK') throw new Error(`Error ${data.code}: ${data.msg}`);
-        return data.data;
+    const {ccy: fromCcy, amount: fromAmt} = this._splitCcy(from);
+    const {ccy: toCcy, amount: toAmt} = this._splitCcy(to);
+    const {amount, direction} = this._calcAmtAndDir(fromAmt, toAmt);
+
+    const {data} = await this._client.post(
+      'price',
+      _.omitBy({
+        type,
+        fromCcy, toCcy,
+        amount, direction,
+        refcode: this.refcode,
+        afftax: this.afftax,
+      })
+    );
+    return data;
+  }
+
+  /**
+   * Receiving information about the order.
+   * @param {String} id Order ID
+   * @param {String} token Security token of order
+   */
+  async getOrder(id, token) {
+    const {data} = await this._client.post('/order', {id, token});
+    return data
+  }
+
+  /**
+   * Emergency Action Choice
+   * @param {String} id Order ID
+   * @param {String} token Security token of order
+   * @param {String} choice EXCHANGE or REFUND
+   * @param {String} address refund address, required if choice="REFUND"
+   */
+  async setEmergency(id, token, choice, address) {
+    const {data} = await this._client.post('/emergency', {id, token, choice, address});
+    return data;
+  }
+
+  /**
+   * Creating exchange orders.
+   * @param {String} from From currency (ex. 0.1 ETH)
+   * @param {String} to To currency (ex. BTC)
+   * @param {String} toAddress A destination address to which the funds will be dispatched upon the successful completion of the Order
+   * @param {'fixed'|'float'} type Order type: fixed or float (def. float)
+   * @param {String} tag This parameter can be omitted by specifying the MEMO or Destination Tag in toAddress separated by a colon.
+   */
+  async createOrder(from, to, toAddress, type = 'float', tag) {
+    if (!from || !to || from.indexOf(' ') + to.indexOf(' ') === -2) {
+      throw new Error(`No required params. Example: {from: '0.1 ETH', to: 'BTC', ...}`);
     }
 
-    /**
-     * Getting a list of all currencies that are available on FixedFloat.com.
-     */
-    async getCurrencies() {
-        return await this._request('GET', 'getCurrencies')
-    }
+    const {ccy: fromCcy, amount: fromAmt} = this._splitCcy(from);
+    const {ccy: toCcy, amount: toAmt} = this._splitCcy(to);
+    const {amount, direction} = this._calcAmtAndDir(fromAmt, toAmt);
 
-    /**
-     * Information about a currency pair with a set amount of funds.
-     * @param {String} from From currency (ex. 0.1 ETH)
-     * @param {String} to To currency (ex. BTC)
-     * @param {'fixed'|'float'} type Order type: fixed or float (def. float)
-     */
-    async getPrice(from, to, type = 'float') {
-        if (!from || !to || from.indexOf(' ') + to.indexOf(' ') === -2) throw new Error(`No required params. Example: {from: '0.1 ETH', to: 'BTC'}`);
-        const body = new URLSearchParams({type});
-        currencyAppend('from', body, from);
-        currencyAppend('to', body, to);
-        return await this._request('POST', 'getPrice', body.toString())
-    }
+    const {data} = await this._client.post(
+      '/create',
+      _.omitBy({
+        type, tag,
+        fromCcy, toCcy,
+        amount, direction,
+        refcode: this.refcode,
+        afftax: this.afftax,
+      }, _.identity)
+    );
 
-    /**
-     * Receiving information about the order.
-     * @param {String} id Order ID
-     * @param {String} token Security token of order
-     */
-    async getOrder(id, token) {
-        const body = new URLSearchParams({id, token});
-        return resolveState(await this._request('GET', 'getOrder', body.toString()));
-    }
-
-    /**
-     * Emergency Action Choice
-     * @param {String} id Order ID
-     * @param {String} token Security token of order
-     * @param {String} choice EXCHANGE or REFUND
-     * @param {String} address refund address, required if choice="REFUND"
-     */
-    async setEmergency(id, token, choice, address) {
-        const body = new URLSearchParams({id, token, choice, address});
-        return await this._request('GET', 'setEmergency', body.toString())
-    }
-
-    /**
-     * Creating exchange orders.
-     * @param {String} from From currency (ex. 0.1 ETH)
-     * @param {String} to To currency (ex. BTC)
-     * @param {String} toAddress A destination address to which the funds will be dispatched upon the successful completion of the Order
-     * @param {'fixed'|'float'} type Order type: fixed or float (def. float)
-     * @param {String} extra This parameter can be omitted by specifying the MEMO or Destination Tag in toAddress separated by a colon.
-     */
-    async createOrder(from, to, toAddress, type = 'float', extra) {
-        if (!from || !to || from.indexOf(' ') + to.indexOf(' ') === -2) throw new Error(`No required params. Example: {from: '0.1 ETH', to: 'BTC', ...}`);
-        const body = new URLSearchParams({type, toAddress});
-        currencyAppend('from', body, from);
-        currencyAppend('to', body, to);
-        if (extra) body.append('extra', extra);
-        return resolveState(await this._request('POST', 'createOrder', body.toString()));
-    }
+    return data;
+  }
 }
 
 module.exports = FixedFloat;
